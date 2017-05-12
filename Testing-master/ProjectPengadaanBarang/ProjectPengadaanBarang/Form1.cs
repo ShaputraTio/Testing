f2e207d5a027d91bdd9a5a791e6aa95d639b726b
elfinsanjaya <veet2017@gmail.com>
2017-05-12 16:08:23 +0700

    Add files via upload

---
+++ Testing-master/ProjectPengadaanBarang/ProjectPengadaanBarang/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectPengadaanBarang
+{
+    public partial class Form1 : Form
+    {    public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FormPersegipanjang22 x = new FormPersegipanjang22();
+      
+            
+            x.Visible = true;
+
+        }
+    }
+}
